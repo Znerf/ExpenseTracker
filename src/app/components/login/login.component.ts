af9d73eea02557af984c63c23eb86b8e3824e629
Sagar <znerfii.a1@gmail.com>
2025-07-11 07:55:44 -0700

    feat: added login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import axios from 'axios';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   email = '';
   password = '';
+  errorMessage = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {}
 
   onLogin() {
-    // TODO: Call login API
-    console.log('Login:', this.email, this.password);
+    this.errorMessage = '';
+    axios.post('http://localhost:8080/api/logins/login', {
+      email: this.email,
+      password: this.password
+    })
+      .then((response) => {
+        if (response.data && (response.data.error )) {
+          this.errorMessage = response.data.error;
+        } else {
+          console.log('Login success:', response.data);
+        }
+        this.cdr.markForCheck();
+      })
+      .catch((error) => {
+        if (error.response && error.response.data && error.response.data.message) {
+          this.errorMessage = error.response.data.message;
+        } else if (error.message) {
+          this.errorMessage = error.message;
+        } else {
+          this.errorMessage = 'Login failed. Please try again.';
+        }
+        this.cdr.markForCheck();
+      });
   }
 
   goToSignup() {
